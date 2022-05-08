2362d2c1404d72482e7dc05a09b9cface2cff45e
satosis <hung0913003358@gmail.com>
2022-05-08 17:35:57 +0700

    update

---
+++ src/Controller/Api/AuthController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Controller\Api;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class AuthController extends AbstractController
+{
+    public function login()
+    {
+        return 2;
+    }
+}
